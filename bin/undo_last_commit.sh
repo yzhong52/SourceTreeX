3f5ec8acca0110d35a0eef3cbb33b602f6043ac2
Yuchen Zhong <yuchen@hopper.com>
2017-01-03 11:41:12 -0500

    Undo Last Commit
    When recreating the branch, also checkout the new branch with `-b`

--- bin/undo_last_commit.sh
+++ bin/undo_last_commit.sh
@@ -6,7 +6,7 @@ current_branch_name=$(git rev-parse --abbrev-ref HEAD)
 
 sh $base_dir"/make_zombie.sh"
 
-git branch "$current_branch_name"
+git checkout -b "$current_branch_name"
 
 git reset --soft HEAD~1
 
