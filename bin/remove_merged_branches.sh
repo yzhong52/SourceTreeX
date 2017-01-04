645c4f5c8f06cac09b89fa672e303eaee900bac6
Yuchen Zhong <yuchen@hopper.com>
2017-01-04 16:34:50 -0500

    Remove Merged Branches
    * Ignore branches that are not from remote `$remote_name` by `grep '^  $remote_name/‘`
    * Fix a few indention problem in other scripts

--- bin/remove_merged_branches.sh
+++ bin/remove_merged_branches.sh
@@ -16,14 +16,14 @@ echo "Branches to check: "$(join_by ', ' $all_branches)
 for branch_name in $all_branches; do
   exists=$(git show-ref refs/heads/$branch_name)
   if [ -n "$exists" ]; then
-	  echo "Delete branches that is merged to '$branch_name'..."
+    echo "Delete branches that is merged to '$branch_name'..."
     git checkout --quiet $branch_name
     git branch --merged | egrep -v "(^\*|master|dev|develop)" | xargs git branch -d --verbose
 
-	  # http://stackoverflow.com/a/6592241/1035008
+    # http://stackoverflow.com/a/6592241/1035008
     if [[ ! -z $remote_name ]]; then
       echo "Delete branches that is merged to '$branch_name' in remote '$remote_name'..."
-    	git branch --remote --merged | egrep -v "(^\*|master|dev|develop)" | sed 's/$remote_name\///' | xargs git push --delete $remote_name
+      git branch --remote --merged | egrep -v "(^\*|master|dev|develop)" | grep '^  $remote_name/' | sed 's/$remote_name\///' | xargs git push --delete $remote_name
     fi
 
   else
