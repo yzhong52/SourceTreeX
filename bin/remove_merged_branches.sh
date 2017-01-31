45a216a8a294d317e741e8a0f5cba08a47ce3d18
Yuchen Zhong <yuchen@hopper.com>
2017-01-31 18:16:08 -0500

    Avoiding deleting the current branch

--- bin/remove_merged_branches.sh
+++ bin/remove_merged_branches.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 echo ""
-echo "Remove Merged Branches Begin... "
 
 remote_name=$1
 
@@ -14,14 +13,17 @@ all_branches=$(echo $all_branches | xargs -n1 | sort -u | xargs)
 
 # http://stackoverflow.com/a/17841619/1035008
 function join_by { local d=$1; shift; echo -n "$1"; shift; printf "%s" "${@/#/$d}"; }
-echo "Branches to check: "$(join_by ', ' $all_branches)
+echo "Remove Merged Branches Begin! Branches to check: "
+echo $(join_by ', ' $all_branches)
 
 for branch_name in $all_branches; do
+  echo "" # Add a line break for each iteration
+
   exists=$(git show-ref refs/heads/$branch_name)
   if [ -n "$exists" ]; then
     echo "Delete branches that is merged to '$branch_name'..."
     git checkout --quiet $branch_name
-    git branch --merged | egrep -v "(^\*|master|dev|develop)" | xargs git branch -d --verbose
+    git branch --merged | egrep -v "(^\*|master|dev|develop|$current_branch_name)" | xargs git branch -d --verbose
 
     # http://stackoverflow.com/a/6592241/1035008
     if [[ ! -z $remote_name ]]; then
@@ -29,14 +31,15 @@ for branch_name in $all_branches; do
       git fetch --prune $remote_name
 
       echo "Delete branches that is merged to '$branch_name' in remote '$remote_name'..."
-      git branch --remote --merged | egrep -v "(^\*|master|dev|develop)" | grep "^  $remote_name/" | sed "s/$remote_name\///" | xargs git push --delete $remote_name
+      git branch --remote --merged | egrep -v "(^\*|master|dev|develop|$current_branch_name)" | grep "^  $remote_name/" | sed "s/$remote_name\///" | xargs git push --delete $remote_name
     fi
 
   else
-    echo "Skipping branch '$branch_name'..."
+    echo "Skipping branch '$branch_name' because it doesn't exist. "
   fi
 done
 
 git checkout --quiet $current_branch_name
 
+echo ""
 echo "Remove Merged Branches Done. "
