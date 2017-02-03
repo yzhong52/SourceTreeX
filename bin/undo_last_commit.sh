f29381ca5846f7ca9f426fc0d29f980f98c57d28
Yuchen Zhong <yuchen@hopper.com>
2017-02-03 11:31:40 -0500

    Remote Tracking branch for current branch
    The make zombie script will remove the remote tracking branch from the current branch. We should add that tracking branch for this undo last commit command.

--- bin/undo_last_commit.sh
+++ bin/undo_last_commit.sh
@@ -8,6 +8,10 @@ sh $base_dir"/make_zombie.sh"
 
 git checkout -b "$current_branch_name"
 
+# The make zombie script will remove the remote tracking branch from the current
+# branch. We should add that tracking branch for this undo last commit command.
+git branch -u origin/"$current_branch_name"
+
 git reset --soft HEAD~1
 
 echo "Done."
