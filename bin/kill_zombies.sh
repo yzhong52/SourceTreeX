37c601f57d3a0c6a65b005fbebe5f0fec6ade1e0
Yuchen Zhong <yuchen@hopper.com>
2017-04-17 14:27:03 -0400

    Kills zombies that is older 30 days

--- bin/kill_zombies.sh
+++ bin/kill_zombies.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+#!/bin/bash 
 
-echo "Kill zombies that are older than 60 days. "
+echo "Kill zombies that are older than 30 days. "
 
 # http://stackoverflow.com/a/10325758/1035008
 for zombie_branch in $(git branch | grep zombie*); do
   
   # http://stackoverflow.com/a/5396755/1035008
-  if [[ -z $(git log -1 --since='60 days ago' -s $zombie_branch) ]]; then
+  if [[ -z $(git log -1 --since='30 days ago' -s $zombie_branch) ]]; then
     git branch -D $zombie_branch
   else 
   	echo $zombie_branch" will live on"
