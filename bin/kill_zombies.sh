a1fe839d43fe1dfc69a234247f762ca0ae79e408
Yuchen Zhong <yuchen@hopper.com>
2017-10-15 11:53:02 -0400

    Update kill zombies script with better logging info

--- bin/kill_zombies.sh
+++ bin/kill_zombies.sh
@@ -1,15 +1,15 @@
 #!/bin/bash 
 
-echo "Kill zombies that are older than 30 days. "
+echo "Kill all zombies that are older than 30 days. "
 
-# http://stackoverflow.com/a/10325758/1035008
+# Delete all branches that are more than X days/weeks old
+# http://stackoverflow.com/a/5396755/1035008
 for zombie_branch in $(git branch | grep zombie*); do
-  
-  # http://stackoverflow.com/a/5396755/1035008
   if [[ -z $(git log -1 --since='30 days ago' -s $zombie_branch) ]]; then
     git branch -D $zombie_branch
+    echo "[Slayed]   "$zombie_branch
   else 
-  	echo $zombie_branch" will live on"
+  	echo "[Survived] "$zombie_branch
   fi
 done
 
