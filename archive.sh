a7d4d343d35b2ae9e18602137941016c916a63e5
Yuchen Zhong <yuchen@hopper.com>
2019-06-23 17:10:13 -0400

    Update readme and screenshots

--- archive.sh
+++ archive.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 cp ~/Library/Application\ Support/SourceTree/actions.plist actions.plist
+
+# `plist` has two formats: binary format and xml format.
+# binary format is more compact and is used by SourceTree by default.
+# However, we convert it to xml here to store in git.
 plutil -convert xml1 actions.plist
